7bc5722396fca781a8558e7664d52908683dae88
hunch1 <taylor1525@hotmail.com>
2024-07-30 23:54:18 -0400

    Assignment2

--- main.cpp
+++ main.cpp
@@ -1,160 +1,178 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
-// Vertex Shader source code
+using namespace std;
+
+// 1-  Define the Vertex Shader and Fragment Shader:
+//     Create a simple vertex and fragment shader to render the shapes.
+
+        //GLSL (OpenGL Shading Language)
 const char* vertexShaderSource = R"glsl(
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    void main()
-    {
-        gl_Position = vec4(aPos, 1.0);
-    }
-)glsl";
-
-// Fragment Shader source code
-const char* fragmentShaderSource = R"glsl(
-    #version 330 core
-    out vec4 FragColor;
-    void main()
-    {
-        FragColor = vec4(1.0, 0.5, 0.2, 1.0); // Orange color
-    }
-)glsl";
-
-void checkCompileErrors(unsigned int shader, std::string type);
-
-int main(void)
+#version 330 core
+layout (location = 0) in vec3 aPos;
+uniform mat4 transform;
+void main()
 {
-    // Initialize the library
-    if (!glfwInit())
-        return -1;
+    gl_Position = transform * vec4(aPos, 1.0);
+}
+)glsl";
+// rain bow gradiant color
+const char* fragmentShaderSource = R"glsl(
+#version 330 core
+out vec4 FragColor;
+uniform float time;
+void main()
+{
+    float r = 0.5 * sin(time + 0.0) + 0.5;
+    float g = 0.5 * sin(time + 2.0) + 0.5;
+    float b = 0.5 * sin(time + 4.0) + 0.5;
+    FragColor = vec4(r, g, b, 1.0);
+}
+)glsl";
 
-    // Set OpenGL version and profile
+// Step 3: Applying Transformations
+        // 1. Create Transformation Matrices:
+        // Use GLM (OpenGL Mathematics) library to create transformation matrices.
+        // 2. Add Uniforms for Transformation Matrices in the Shader:
+        // Update the vertex shader to accept a transformation matrix.
+        // 3. Apply Transformations:
+
+void processInput(GLFWwindow* window, glm::mat4& transform) {
+    float translationSpeed = 0.01f; // Speed of translation
+    float rotationSpeed = glm::radians(30.0f); // 30 degrees rotation
+    float scaleSpeed = 0.4f; // Scaling factor
+
+//TODO : IF OTHER INPUT SAY INVALID !!
+
+    // Translation 
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        transform = glm::translate(transform, glm::vec3(0.0f, translationSpeed, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        transform = glm::translate(transform, glm::vec3(0.0f, -translationSpeed, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        transform = glm::translate(transform, glm::vec3(-translationSpeed, 0.0f, 0.0f));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        transform = glm::translate(transform, glm::vec3(translationSpeed, 0.0f, 0.0f));
+
+    // Rotation 
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        transform = glm::rotate(transform, rotationSpeed, glm::vec3(0.0f, 0.0f, 1.0f));
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        transform = glm::rotate(transform, -rotationSpeed, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    // Scaling 
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+        transform = glm::scale(transform, glm::vec3(scaleSpeed, scaleSpeed, scaleSpeed));
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
+        transform = glm::scale(transform, glm::vec3(1/scaleSpeed, 1/scaleSpeed, 1/scaleSpeed));
+
+    // Close the window
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    glfwInit();
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
-    if (!window)
-    {
+// window creation
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Assignment 2", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed to initial window" << endl;
         glfwTerminate();
         return -1;
     }
-
-    // Make the window's context current
     glfwMakeContextCurrent(window);
+    glewInit(); // OpenGL Extension Wrangler Library
 
-    // Initialize GLEW
-    if (glewInit() != GLEW_OK)
-    {
-        std::cerr << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }
 
-    // Vertex Shader
+//  2- Compile and Link the Shaders:
+//     Compile the vertex and fragment shaders and link them into a shader program.
+  
+     // vertext shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); //vertexShaderSource
     glCompileShader(vertexShader);
-    checkCompileErrors(vertexShader, "VERTEX");
 
-    // Fragment Shader
+    //fragment shader
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    checkCompileErrors(fragmentShader, "FRAGMENT");
 
-    // Shader Program
-    unsigned int shaderProgram = glCreateProgram();
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL); //fragmentShaderSource
+    glCompileShader(fragmentShader);
+
+    // Shader program
+    unsigned int shaderProgram = glCreateProgram(); // attaching vertex and fragment shaders to the program
+    
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    checkCompileErrors(shaderProgram, "PROGRAM");
 
-    // Delete shaders as they're linked into our program now and no longer necessary
+    glLinkProgram(shaderProgram);
+
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // Set up vertex data (and buffer(s)) and configure vertex attributes
+// 3. Define the Vertex Data for a Triangle:
+
     float vertices[] = {
-         0.0f,  0.5f, 0.0f,  // top 
-        -0.5f, -0.5f, 0.0f,  // bottom left
-         0.5f, -0.5f, 0.0f   // bottom right
+        -0.5f, -0.5f, 0.0f,  // Bottom left
+         0.5f, -0.5f, 0.0f,  // Bottom right
+         0.0f,  0.5f, 0.0f   // Top
     };
 
+//4. Set Up the Vertex Array Object (VAO), Vertex Buffer Object (VBO), and Element Buffer Object (EBO):
+        //Low Vertex Count so no EBO
+
     unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    // Bind Vertex Array Object
+    glGenVertexArrays(1, &VAO); //Generate and Bind VAO
+    glGenBuffers(1, &VBO);   // able to Stores one triangle object
     glBindVertexArray(VAO);
-
-    // Bind and set vertex buffer(s)
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Configure vertex attributes
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
     glEnableVertexAttribArray(0);
 
-    // Unbind buffer and array
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    glm::mat4 transform = glm::mat4(1.0f);
+    unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+    unsigned int timeLoc = glGetUniformLocation(shaderProgram, "time"); // to create rain bow effect dynamically
 
-    // Render loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Clear the color buffer
+// 5. Render the Shapes
+// Render the Shapes
+    while (!glfwWindowShouldClose(window)) {
+         
+         //give window state and transform
+        processInput(window, transform);
+
+        glClearColor(0.529f, 0.808f, 0.980f, 1.0f);  // Sky blue color
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // Use the shader program and bind the VAO
         glUseProgram(shaderProgram);
+        //// Sets the value of the  variable transform in the shader program
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+        glUniform1f(timeLoc, glfwGetTime());
         glBindVertexArray(VAO);
-
-        // Draw the triangle
+        //Draws the triangles based on the currently bound VAO and its associated VBO
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // Unbind the VAO
-        glBindVertexArray(0);
+       
 
-        // Swap front and back buffers
+        //Swaps the front and back buffers.
         glfwSwapBuffers(window);
-
-        // Poll for and process events
+        //Processes events
         glfwPollEvents();
     }
 
-    // Deallocate resources
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
 
-    // Terminate GLFW
     glfwTerminate();
     return 0;
-}
-
-void checkCompileErrors(unsigned int shader, std::string type)
-{
-    int success;
-    char infoLog[1024];
-    if (type != "PROGRAM")
-    {
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-        if (!success)
-        {
-            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-            std::cerr << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-        }
-    }
-    else
-    {
-        glGetProgramiv(shader, GL_LINK_STATUS, &success);
-        if (!success)
-        {
-            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-            std::cerr << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
-        }
-    }
 }
